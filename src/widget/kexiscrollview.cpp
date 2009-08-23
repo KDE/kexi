KDE/kexi

commit a435c3f756fe5c4e2fe625a4c50cd6fe4aafb554

- Table View works, tables can be opened in the navigator
svn path=/trunk/koffice/; revision=683869

src/widget/kexiscrollview.cpp
@@ -82,7 +82,8 @@ KexiScrollView::KexiScrollView(QWidget *parent, bool preview)
 		refreshContentsSizeLater(true, true);
 //! @todo allow to hide navigator
 		updateScrollBars();
-		m_scrollViewNavPanel = new KexiRecordNavigator(this, leftMargin(), "nav");
+		m_scrollViewNavPanel = new KexiRecordNavigator(this, leftMargin());
+		m_scrollViewNavPanel->setObjectName("nav");
 		m_scrollViewNavPanel->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Preferred);
 	}
 }
@@ -101,10 +102,13 @@ KexiScrollView::setWidget(QWidget *w)
 void
 KexiScrollView::setRecordNavigatorVisible(bool visible)
 {
-	if(/*m_scrollViewNavPanel->isVisible() &&*/ !visible)
-		m_scrollViewNavPanel->hide();
+	if(/*m_scrollViewNavPanel->isVisible() &&*/ !visible) {
+		if (m_scrollViewNavPanel)
+			m_scrollViewNavPanel->hide();
+	}
 	else if(visible)  {
-		m_scrollViewNavPanel->show();
+		if (m_scrollViewNavPanel)
+			m_scrollViewNavPanel->hide();
 		updateNavPanelGeometry();
 	}
 }

src/core/KexiMainWindowIface.h
@@ -197,8 +197,9 @@ class KEXICORE_EXPORT KexiMainWindowIface : /*public KexiMdiMainFrm,*/ public Ke
 		/*! Closes window \a window. If window's data (see KexiWindow::isDirty()) is unsaved,
 		 used will be asked if saving should be perforemed.
 		 \return true on successull closing or false on closing error.
-		 If closing was cancelled by user, cancelled is returned. */
-		virtual tristate closeWindow(KexiWindow *dlg) = 0;
+		 If closing was cancelled by user, cancelled is returned.
+		 If \a window is 0, the current one will be closed. */
+		virtual tristate closeWindow(KexiWindow *window) = 0;
 
 		/*! Displays a window for entering object's name and title.
 		 Used on new object saving.

src/core/KexiWindow.cpp
@@ -66,7 +66,7 @@ class KexiWindow::Private
 		inline void setIndexForView( Kexi::ViewMode mode, int idx )
 			{ indicesForViews.insert((int)mode, idx); }
 		
-		Kexi::ViewMode supportedViewModes;
+		Kexi::ViewModes supportedViewModes;
 		Kexi::ViewModes openedViewModes;
 		Kexi::ViewMode currentViewMode;
 		
@@ -108,6 +108,7 @@ KexiWindow::KexiWindow(/*const QString &caption, */ QWidget *parent,
 {
 	d->part = &part;
 	d->item = &item;
+	d->supportedViewModes = supportedViewModes;
 #ifdef __GNUC__
 #warning todo KexiWindow: caption, parent?
 #else
@@ -230,6 +231,7 @@ KexiPart::Item *KexiWindow::partItem() const
 
 bool KexiWindow::supportsViewMode( Kexi::ViewMode mode ) const
 {
+kexidbg<<d->supportedViewModes << endl;
 	return d->supportedViewModes & mode;
 }
 
@@ -425,14 +427,16 @@ tristate KexiWindow::switchToViewMode(
 			return res;
 	}
 
-	kDebug() << "KexiWindow::switchToViewMode()" << endl;
+	kexidbg << "KexiWindow::switchToViewMode()" << endl;
 	bool dontStore = false;
 	KexiView *view = selectedView();
 
 	if (d->currentViewMode == newViewMode)
 		return true;
-	if (!supportsViewMode(newViewMode))
+	if (!supportsViewMode(newViewMode)) {
+		kexiwarn << "! KexiWindow::supportsViewMode("<<Kexi::nameForViewMode(newViewMode)<<")"<<endl;
 		return false;
+	}
 
 	if (view) {
 		res = true;
@@ -468,7 +472,7 @@ tristate KexiWindow::switchToViewMode(
 		KexiUtils::removeWaitCursor();
 		if (!newView) {
 			//js TODO error?
-			kDebug() << "Switching to mode " << newViewMode << " failed. Previous mode "
+			kexiwarn << "Switching to mode " << newViewMode << " failed. Previous mode "
 				<< d->currentViewMode << " restored." << endl;
 			return false;
 		}
@@ -486,7 +490,7 @@ tristate KexiWindow::switchToViewMode(
 	if (!res) {
 		removeView(newViewMode);
 		delete newView;
-		kDebug() << "Switching to mode " << newViewMode << " failed. Previous mode "
+		kexiwarn << "Switching to mode " << newViewMode << " failed. Previous mode "
 			<< d->currentViewMode << " restored." << endl;
 		return false;
 	}
@@ -502,7 +506,7 @@ tristate KexiWindow::switchToViewMode(
 	if (!res) {
 		removeView(newViewMode);
 		delete newView;
-		kDebug() << "Switching to mode " << newViewMode << " failed. Previous mode "
+		kexiwarn << "Switching to mode " << newViewMode << " failed. Previous mode "
 			<< prevViewMode << " restored." << endl;
 		const Kexi::ObjectStatus status(*this);
 		setStatus(KexiMainWindowIface::global()->project()->dbConnection(), 
@@ -618,6 +622,12 @@ void KexiWindow::updateCaption()
 #else
 #pragma WARNING( TODO KexiWindow::updateCaption() )
 #endif
+	if (isDirty()) {
+		setWindowTitle(fullCapt+"*");
+	}
+	else {
+		setWindowTitle(fullCapt);
+	}
 #if 0 //TODO
 	if (isDirty()) {
 		QStackedWidget::setCaption(fullCapt+"*");
@@ -663,7 +673,7 @@ tristate KexiWindow::storeNewData()
 		//this part's ID is not stored within kexi__parts:
 		KexiDB::TableSchema *ts =
 			KexiMainWindowIface::global()->project()->dbConnection()->tableSchema("kexi__parts");
-		kDebug() << "KexiWindow::storeNewData(): schema: " << ts << endl;
+		kexidbg << "KexiWindow::storeNewData(): schema: " << ts << endl;
 		if (!ts)
 			return false;
 
@@ -688,12 +698,12 @@ tristate KexiWindow::storeNewData()
 		}
 
 		KexiDB::FieldList *fl = ts->subList("p_id", "p_name", "p_mime", "p_url");
-		kDebug() << "KexiWindow::storeNewData(): fieldlist: " 
+		kexidbg << "KexiWindow::storeNewData(): fieldlist: " 
 			<< (fl ? fl->debugString() : QString()) << endl;
 		if (!fl)
 			return false;
 
-		kDebug() << part()->info()->ptr()->untranslatedGenericName() << endl;
+		kexidbg << part()->info()->ptr()->untranslatedGenericName() << endl;
 //		QStringList sl = part()->info()->ptr()->propertyNames();
 //		for (QStringList::ConstIterator it=sl.constBegin();it!=sl.constEnd();++it)
 //			kexidbg << *it << " " << part()->info()->ptr()->property(*it).toString() <<  endl;
@@ -706,10 +716,10 @@ tristate KexiWindow::storeNewData()
 				QVariant("http://www.koffice.org/kexi/" /*always ok?*/)))
 			return false;
 
-		kDebug() << "KexiWindow::storeNewData(): insert success!" << endl;
+		kexidbg << "KexiWindow::storeNewData(): insert success!" << endl;
 		part()->info()->setProjectPartID( p_id );
 			//(int) project()->dbConnection()->lastInsertedAutoIncValue("p_id", "kexi__parts"));
-		kDebug() << "KexiWindow::storeNewData(): new id is: " 
+		kexidbg << "KexiWindow::storeNewData(): new id is: " 
 			<< part()->info()->projectPartID()  << endl;
 
 		part()->info()->setIdStoredInPartDatabase(true);
@@ -766,7 +776,7 @@ tristate KexiWindow::storeData(bool dontAsk)
 void KexiWindow::activate()
 {
 	KexiView *v = selectedView();
-	//kDebug() << "focusWidget(): " << focusWidget()->name() << endl;
+	//kexidbg << "focusWidget(): " << focusWidget()->name() << endl;
 #ifdef __GNUC__
 #warning TODO KexiWindow::activate() OK instead of focusedChildWidget()?
 #else

src/core/kexipart.cpp
@@ -197,7 +197,7 @@ KAction* Part::createSharedAction(Kexi::ViewMode mode, const QString &text,
 {
 	GUIClient *instanceGuiClient = d->instanceGuiClients.value((int)mode);
 	if (!instanceGuiClient) {
-		kDebug() << "KexiPart::createSharedAction(): no gui client for mode " << mode << "!" << endl;
+		kexidbg << "KexiPart::createSharedAction(): no gui client for mode " << mode << "!" << endl;
 		return 0;
 	}
 	return KexiMainWindowIface::global()->createSharedAction(text, pix_name, cut, name, 
@@ -230,7 +230,7 @@ KAction* Part::createSharedPartToggleAction(const QString &text,
 {
 	GUIClient *instanceGuiClient = d->instanceGuiClients[mode];
 	if (!instanceGuiClient) {
-		kDebug() << "KexiPart::createSharedAction(): no gui client for mode " << mode << "!" << endl;
+		kexidbg << "KexiPart::createSharedAction(): no gui client for mode " << mode << "!" << endl;
 		return 0;
 	}
 	return instanceGuiClient->actionCollection()->action(name, classname);
@@ -255,7 +255,7 @@ void Part::setActionAvailable(const char *action_name, bool avail)
 	KexiMainWindowIface::global()->setActionAvailable(action_name, avail);
 }
 
-KexiWindow* Part::openInstance(KexiPart::Item &item, Kexi::ViewMode viewMode,
+KexiWindow* Part::openInstance(QWidget* parent, KexiPart::Item &item, Kexi::ViewMode viewMode,
 	QMap<QString,QString>* staticObjectArgs)
 {
 	//now it's the time for creating instance actions
@@ -265,7 +265,7 @@ KexiWindow* Part::openInstance(KexiPart::Item &item, Kexi::ViewMode viewMode,
 	}
 
 	d->status.clearStatus();
-	KexiWindow *window = new KexiWindow(KexiMainWindowIface::global()->thisWidget(),
+	KexiWindow *window = new KexiWindow(parent,
 		d->supportedViewModes, *this, item);
 
 	KexiDB::SchemaData sdata(d->info->projectPartID());
@@ -329,12 +329,14 @@ KexiWindow* Part::openInstance(KexiPart::Item &item, Kexi::ViewMode viewMode,
 			window->close();
 			delete window;
 			//try in text mode
-			return openInstance(item, Kexi::TextViewMode, staticObjectArgs);
+			return openInstance(parent, item, Kexi::TextViewMode, staticObjectArgs);
 		}
 		else if (false == askForOpeningInTextModeRes) {
 			delete window->schemaData(); //old one
 			window->close();
 			delete window;
+			kexiwarn << "Part::openInstance() !window, cannot switch to a view mode " << 
+				Kexi::nameForViewMode(viewMode) << endl;
 			return 0;
 		}
 		//the window has an error info
@@ -347,6 +349,8 @@ KexiWindow* Part::openInstance(KexiPart::Item &item, Kexi::ViewMode viewMode,
 		d->status = window->status();
 		window->close();
 		delete window;
+		kexiwarn << "Part::openInstance() !window, switching to view mode failed, " << 
+			Kexi::nameForViewMode(viewMode) << endl;
 		return 0;
 	}
 	window->registerWindow(); //ok?
@@ -367,6 +371,7 @@ KexiWindow* Part::openInstance(KexiPart::Item &item, Kexi::ViewMode viewMode,
 	if (window->selectedView())
 		window->selectedView()->setDirty( d->newObjectsAreDirty ? item.neverSaved() : false );
 	
+	kexidbg << "Part::openInstance() window returned."<<endl;
 	return window;
 }
 

src/core/kexipart.h
@@ -103,8 +103,9 @@ class KEXICORE_EXPORT Part : public QObject
 //! @todo make it protected, outside world should use KexiProject
 		/*! "Opens" an instance that the part provides, pointed by \a item in a mode \a viewMode. 
 		 \a viewMode is one of Kexi::ViewMode enum. 
-		 \a staticObjectArgs can be passed for static Kexi Parts. */
-		KexiWindow* openInstance(KexiPart::Item &item, 
+		 \a staticObjectArgs can be passed for static Kexi Parts.
+		 The new widget will be a child of \a parent. */
+		KexiWindow* openInstance(QWidget* parent, KexiPart::Item &item, 
 			Kexi::ViewMode viewMode = Kexi::DataViewMode, QMap<QString,QString>* staticObjectArgs = 0);
 
 //! @todo make it protected, outside world should use KexiProject

src/core/kexiproject.cpp
@@ -633,13 +633,16 @@ KexiPart::Item*
 KexiProject::itemForMimeType(const QString &mimeType, const QString &name)
 {
 	KexiPart::ItemDict *dict = itemsForMimeType(mimeType);
-	if (!dict)
+	if (!dict) {
+		kexiwarn << "KexiProject::itemForMimeType() no mimetype="<<mimeType<<endl;
 		return 0;
+	}
 	const QString l_name = name.toLower();
 	for (KexiPart::ItemDictIterator it( *dict ); it.current(); ++it) {
 		if (it.current()->name().toLower()==l_name)
 			return it.current();
 	}
+	kexiwarn << "KexiProject::itemForMimeType() no name="<<name<<endl;
 	return 0;
 }
 
@@ -714,12 +717,14 @@ KexiPart::Part *KexiProject::findPartFor(KexiPart::Item& item)
 	clearError();
 	KexiDB::MessageTitle et(this);
 	KexiPart::Part *part = Kexi::partManager().partForMimeType(item.mimeType());
-	if (!part)
+	if (!part) {
+		kexiwarn<<"KexiProject::findPartFor() !part: " << item.mimeType() <<endl;
 		setError(&Kexi::partManager());
+	}
 	return part;
 }
 
-KexiWindow* KexiProject::openObject(KexiPart::Item& item, 
+KexiWindow* KexiProject::openObject(QWidget* parent, KexiPart::Item& item, 
 	Kexi::ViewMode viewMode, QMap<QString,QString>* staticObjectArgs)
 {
 	clearError();
@@ -730,7 +735,7 @@ KexiWindow* KexiProject::openObject(KexiPart::Item& item,
 	KexiPart::Part *part = findPartFor(item);
 	if (!part)
 		return 0;
-	KexiWindow *window  = part->openInstance(item, viewMode, staticObjectArgs);
+	KexiWindow *window  = part->openInstance(parent, item, viewMode, staticObjectArgs);
 	if (!window) {
 		if (part->lastOperationStatus().error())
 			setError(i18n("Opening object \"%1\" failed.", item.name())+"<br>"
@@ -741,11 +746,11 @@ KexiWindow* KexiProject::openObject(KexiPart::Item& item,
 	return window;
 }
 
-KexiWindow* KexiProject::openObject(const QString &mimeType, 
+KexiWindow* KexiProject::openObject(QWidget* parent, const QString &mimeType, 
 	const QString& name, Kexi::ViewMode viewMode)
 {
 	KexiPart::Item *it = itemForMimeType(mimeType, name);
-	return it ? openObject(*it, viewMode) : 0;
+	return it ? openObject(parent, *it, viewMode) : 0;
 }
 
 bool KexiProject::checkWritable()

src/core/kexiproject.h
@@ -175,12 +175,13 @@ class KEXICORE_EXPORT KexiProject : public QObject, public KexiDB::Object
 
 		/*! Opens object pointed by \a item in a view \a viewMode.
 		 \a staticObjectArgs can be passed for static object 
-		 (only works when part for this item is of type KexiPart::StaticPart) */
-		KexiWindow* openObject(KexiPart::Item& item, 
+		 (only works when part for this item is of type KexiPart::StaticPart).
+		 The new widget will be a child of \a parent. */
+		KexiWindow* openObject(QWidget* parent, KexiPart::Item& item, 
 			Kexi::ViewMode viewMode = Kexi::DataViewMode, QMap<QString,QString>* staticObjectArgs = 0);
 
 		//! For convenience
-		KexiWindow* openObject(const QString &mimeType, 
+		KexiWindow* openObject(QWidget* parent, const QString &mimeType, 
 			const QString& name, Kexi::ViewMode viewMode = Kexi::DataViewMode);
 
 		/*! Remove a part instance pointed by \a item.

src/core/kexistaticpart.cpp
@@ -54,7 +54,7 @@ StaticPart::~StaticPart()
 }
 
 KexiView* StaticPart::createView(QWidget *parent, KexiWindow* window, 
-	KexiPart::Item &item, int viewMode)
+	KexiPart::Item &item, Kexi::ViewMode viewMode)
 {
 	Q_UNUSED(parent);
 	Q_UNUSED(window);

commit 6946f523a69b8eda3111a59ac1f875d20894fff1

Fixed typo introduced in r683869, causing hiding the record navigator in forms
svn path=/trunk/koffice/; revision=1014827

src/widget/kexiscrollview.cpp
@@ -116,7 +116,7 @@ KexiScrollView::setRecordNavigatorVisible(bool visible)
             m_scrollViewNavPanel->hide();
     } else if (visible)  {
         if (m_scrollViewNavPanel)
-            m_scrollViewNavPanel->hide();
+            m_scrollViewNavPanel->show();
         updateNavPanelGeometry();
     }
 }