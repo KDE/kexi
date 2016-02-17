KDE/kexi

commit 4a1f5a16532e453e798e77854e514e0c34f8127c

Update for the recent addField/insertField API changes in KDb

src/core/kexiproject.cpp
@@ -501,12 +501,12 @@ bool KexiProject::createInternalStructures(bool insideTransaction)
 
     KDbInternalTableSchema *t_blobs = new KDbInternalTableSchema("kexi__blobs");
     t_blobs->addField(new KDbField("o_id", KDbField::Integer,
-                                        KDbField::PrimaryKey | KDbField::AutoInc, KDbField::Unsigned))
-    .addField(new KDbField("o_data", KDbField::BLOB))
-    .addField(new KDbField("o_name", KDbField::Text))
-    .addField(new KDbField("o_caption", KDbField::Text))
-    .addField(new KDbField("o_mime", KDbField::Text, KDbField::NotNull))
-    .addField(new KDbField("o_folder_id",
+                                        KDbField::PrimaryKey | KDbField::AutoInc, KDbField::Unsigned));
+    t_blobs->addField(new KDbField("o_data", KDbField::BLOB));
+    t_blobs->addField(new KDbField("o_name", KDbField::Text));
+    t_blobs->addField(new KDbField("o_caption", KDbField::Text));
+    t_blobs->addField(new KDbField("o_mime", KDbField::Text, KDbField::NotNull));
+    t_blobs->addField(new KDbField("o_folder_id",
                                 KDbField::Integer, 0, KDbField::Unsigned) //references kexi__gallery_folders.f_id
               //If null, the BLOB only points to virtual "All" folder
               //WILL BE USED in Kexi >=2.0
@@ -559,10 +559,10 @@ bool KexiProject::createInternalStructures(bool insideTransaction)
     KDbInternalTableSchema *t_parts = new KDbInternalTableSchema("kexi__parts");
     t_parts->addField(
         new KDbField("p_id", KDbField::Integer, KDbField::PrimaryKey | KDbField::AutoInc, KDbField::Unsigned)
-    )
-    .addField(new KDbField("p_name", KDbField::Text))
-    .addField(new KDbField("p_mime", KDbField::Text))
-    .addField(new KDbField("p_url", KDbField::Text));
+    );
+    t_parts->addField(new KDbField("p_name", KDbField::Text));
+    t_parts->addField(new KDbField("p_mime", KDbField::Text));
+    t_parts->addField(new KDbField("p_url", KDbField::Text));
 
     const tristate containsKexi__partsTable = d->connection->drv_containsTable("kexi__parts");
     if (~containsKexi__partsTable) {
@@ -606,10 +606,10 @@ bool KexiProject::createInternalStructures(bool insideTransaction)
 
     // User data storage
     KDbInternalTableSchema *t_userdata = new KDbInternalTableSchema("kexi__userdata");
-    t_userdata->addField(new KDbField("d_user", KDbField::Text, KDbField::NotNull))
-        .addField(new KDbField("o_id", KDbField::Integer, KDbField::NotNull, KDbField::Unsigned))
-        .addField(new KDbField("d_sub_id", KDbField::Text, KDbField::NotNull | KDbField::NotEmpty))
-        .addField(new KDbField("d_data", KDbField::LongText));
+    t_userdata->addField(new KDbField("d_user", KDbField::Text, KDbField::NotNull));
+    t_parts->addField(new KDbField("o_id", KDbField::Integer, KDbField::NotNull, KDbField::Unsigned));
+    t_parts->addField(new KDbField("d_sub_id", KDbField::Text, KDbField::NotNull | KDbField::NotEmpty));
+    t_parts->addField(new KDbField("d_data", KDbField::LongText));
 
     const tristate containsKexi__userdataTable = d->connection->drv_containsTable("kexi__userdata");
     if (~containsKexi__userdataTable) {

CMakeLists.txt
@@ -226,7 +226,7 @@ endif ()
 ##
 ## Test for KDb
 ##
-find_package(KDb 2.96.1 REQUIRED)
+find_package(KDb 2.96.2 REQUIRED)
 macro_log_feature(KDb_FOUND "KDb" "A database connectivity and creation framework"
                             "http://community.kde.org/KDb" FALSE "" "Required by Kexi and its plugins")
 

src/migration/keximigrate.cpp
@@ -333,10 +333,9 @@ bool KexiMigrate::performImport(Kexi::ObjectStatus* result)
                         break;
                     }
                     KDbField* f = destConn->setupField(data);
-                    if (f)
-                        t->addField(f);
-                    else
+                    if (!f || !t->addField(f)) {
                         ok = false;
+                    }
                 }
                 if (ok)
                     ok = destConn->drv_createTable(*t);

src/migration/mdb/src/keximdb/mdbmigrate.cpp
@@ -174,7 +174,11 @@ bool MDBMigrate::drv_readTableSchema(const QString& originalName,
 
         //qDebug() << "size" << col->col_size << "type" << type(col->col_type);
         fld->setCaption(fldName);
-        tableSchema.addField(fld);
+        if (!tableSchema.addField(fld)) {
+            delete fld;
+            tableSchema.clear();
+            return false;
+        }
     }
 
     getPrimaryKey(&tableSchema, tableDef);
@@ -410,27 +414,33 @@ bool MDBMigrate::getPrimaryKey(KDbTableSchema* table, MdbTableDef* tableDef)
     // MDBTools counts columns from 1 - subtract 1 where necessary
     KDbIndexSchema* p_idx = new KDbIndexSchema(table);
 
+    bool ok = true;
     for (unsigned int i = 0; i < idx->num_keys; i++) {
         key_col_num[i] = idx->key_col_num[i];
         qDebug() << "key" << i + 1 << " col " << key_col_num[i]
                  << table->field(idx->key_col_num[i] - 1)->name();
-        p_idx->addField(table->field(idx->key_col_num[i] - 1));
+        if (!p_idx->addField(table->field(idx->key_col_num[i] - 1))) {
+            delete p_idx;
+            ok = false;
+            break;
+        }
     }
 
-    //qDebug() << *p_idx;
-
-    // ... and add it to the table definition
-    // but only if the PK has only one field, so far :o(
-
-    KDbField *f;
-    if (idx->num_keys == 1 && (f = table->field(idx->key_col_num[0] - 1))) {
-        f->setPrimaryKey(true);
-    } else {
-        //! @todo: How to add a composite PK to a KDbTableSchema?
+    if (ok) {
+        //qDebug() << *p_idx;
+    
+        // ... and add it to the table definition
+        // but only if the PK has only one field, so far :o(
+    
+        KDbField *f;
+        if (idx->num_keys == 1 && (f = table->field(idx->key_col_num[0] - 1))) {
+            f->setPrimaryKey(true);
+        } else {
+            //! @todo: How to add a composite PK to a KDbTableSchema?
+        }
     }
-
     mdb_free_indices(tableDef->indices);
-    return true;
+    return ok;
 }
 
 bool MDBMigrate::drv_getTableSize(const QString& table, quint64& size)

src/migration/mysql/mysqlmigrate.cpp
@@ -113,6 +113,7 @@ bool MySQLMigrate::drv_readTableSchema(
     unsigned int numFlds = mysql_num_fields(res);
     MYSQL_FIELD *fields = mysql_fetch_fields(res);
 
+    bool ok = true;
     for (unsigned int i = 0; i < numFlds; i++) {
         QString fldName(fields[i].name);
         QString fldID(KDb::stringToIdentifier(fldName.toLower()));
@@ -127,10 +128,15 @@ bool MySQLMigrate::drv_readTableSchema(
         fld->setCaption(fldName);
         getConstraints(fields[i].flags, fld);
         getOptions(fields[i].flags, fld);
-        tableSchema.addField(fld);
+        if (!tableSchema.addField(fld)) {
+            delete fld;
+            tableSchema.clear();
+            ok = false;
+            break;
+        }
     }
     mysql_free_result(res);
-    return true;
+    return ok;
 }
 
 

src/migration/pqxx/pqxxmigrate.cpp
@@ -88,6 +88,7 @@ bool PqxxMigrate::drv_readTableSchema(
     if (!query("select * from " + drv_escapeIdentifier(originalName) + " limit 1"))
         return false;
     //Loop round the fields
+    bool ok = true;
     for (int i = 0; i < m_res->columns(); i++) {
         QString fldName(m_res->column_name(i));
         KDbField::Type fldType = type(m_res->column_type(i), fldName);
@@ -100,10 +101,15 @@ bool PqxxMigrate::drv_readTableSchema(
         f->setPrimaryKey(primaryKey(toid, i));
         f->setUniqueKey(uniqueKey(toid, i));
         f->setAutoIncrement(autoInc(toid, i));//This should be safe for all field types
-        tableSchema.addField(f);
+        if (!tableSchema.addField(f)) {
+            delete f;
+            tableSchema.clear();
+            ok = false;
+            break;
+        }
         //qDebug() << "Added field [" << f->name() << "] type [" << f->typeName() << ']';
     }
-    return true;
+    return ok;
 }
 
 //==================================================================================

src/migration/spreadsheet/spreadsheetmigrate.cpp
@@ -127,6 +127,7 @@ bool SpreadsheetMigrate::drv_readTableSchema(const QString& originalName, KexiDB
   
   Calligra::Sheets::Cell *cell;
   
+  bool ok = true;
   forever {
       cell = new Calligra::Sheets::Cell(sheet, col, row);
       if (cell->isEmpty()) {
@@ -149,12 +150,17 @@ bool SpreadsheetMigrate::drv_readTableSchema(const QString& originalName, KexiDB
       }
       KexiDB::Field *fld = new KexiDB::Field(fieldName, KexiDB::Field::Text);
       fld->setCaption(fieldCaption);
-      tableSchema.addField( fld );
+      if (!tableSchema.addField( fld )) {
+          return fld;
+          tableSchema.clear();
+          ok = false;
+          break;
+      }
       qDebug() << fieldName;
       col++;
   }
   
-  return true;
+  return ok;
 }
 
 bool SpreadsheetMigrate::drv_readFromTable(const QString & tableName)

src/migration/sybase/sybasemigrate.cpp
@@ -83,53 +83,59 @@ bool SybaseMigrate::drv_readTableSchema(
     if (!query(sqlStatement))
         return false;
 
+    bool ok = true;
     unsigned int numFlds = dbnumcols(d->dbProcess);
     QVector<KDbField*> fieldVector;
     for (unsigned int i = 1; i <= numFlds; i++) {
         //  dblib indexes start from 1
-        DBCOL* colInfo = new DBCOL;
-        if (dbcolinfo(d->dbProcess, CI_REGULAR, i , 0, colInfo) != SUCCEED) {
+        DBCOL colInfo;
+        if (dbcolinfo(d->dbProcess, CI_REGULAR, i , 0, &colInfo) != SUCCEED) {
             return false;
         }
 
-        QString fldName(dbcolname(d->dbProcess, i));
-        QString fldID(KDb::stringToIdentifier(fldName));
+        const QString fldName(dbcolname(d->dbProcess, i));
+        const QString fldID(KDb::stringToIdentifier(fldName));
 
         KDbField *fld =
             new KDbField(fldID, type(originalName, dbcoltype(d->dbProcess, i)));
         fld->setCaption(fldName);
-        fld->setAutoIncrement(colInfo->Identity == true ? true : false);
-        fld->setNotNull(colInfo->Null == false ? true : false);
+        fld->setAutoIncrement(colInfo.Identity == true ? true : false);
+        fld->setNotNull(colInfo.Null == false ? true : false);
 
         // collect the fields for post-processing
         fieldVector.append(fld);
-        tableSchema.addField(fld);
-
+        if (!tableSchema.addField(fld)) {
+            delete fld;
+            tableSchema.clear();
+            ok = false;
+            break;
+        }
         //qDebug() << fld->caption() << "No.of fields in tableSchema" << tableSchema.fieldCount();
-        delete colInfo;
     }
 
-    // read all the indexes on this table
-    QList<KDbIndexSchema*> indexList = readIndexes(originalName, tableSchema);
-    foreach(KDbIndexSchema* index, indexList) {
-        // We are only looking for indexes on single fields
-        // **kexi limitation**
-        // generalise this when we get full support for indexes
-        if (index->fieldCount() != 1) {
-            continue;
-        }
-        KDbField* fld = index->field(0);
+    if (ok) {
+        // read all the indexes on this table
+        QList<KDbIndexSchema*> indexList = readIndexes(originalName, tableSchema);
+        foreach(KDbIndexSchema* index, indexList) {
+            // We are only looking for indexes on single fields
+            // **kexi limitation**
+            // generalise this when we get full support for indexes
+            if (index->fieldCount() != 1) {
+                continue;
+            }
+            KDbField* fld = index->field(0);
 
-        if (!fld)
-            return false;
+            if (!fld)
+                return false;
 
-        if (index->isPrimaryKey()) {
-            fld->setPrimaryKey(true);
-            tableSchema.setPrimaryKey(index);
-        } else if (index->isUnique()) {
-            fld->setUniqueKey(true);
-        } else {
-            fld->setIndexed(true);
+            if (index->isPrimaryKey()) {
+                fld->setPrimaryKey(true);
+                tableSchema.setPrimaryKey(index);
+            } else if (index->isUnique()) {
+                fld->setUniqueKey(true);
+            } else {
+                fld->setIndexed(true);
+            }
         }
     }
     return true;
@@ -515,6 +521,8 @@ QList<KDbIndexSchema*> KexiMigration::SybaseMigrate::readIndexes(const QString& 
             sqlStatement = QString("Select index_col('%1',%2, %3 )").arg(drv_escapeIdentifier(tableName)).arg(indexId).arg(i);
 
             if (!query(sqlStatement)) {
+                delete indexSchema;
+                qDeleteAll(indexList);
                 return QList<KDbIndexSchema*>();
             }
 
@@ -522,7 +530,11 @@ QList<KDbIndexSchema*> KexiMigration::SybaseMigrate::readIndexes(const QString& 
                 // only one row is expected
                 QString fieldName = value(0);
                 //qDebug() << fieldName;
-                indexSchema->addField(fieldHash[fieldName]);
+                if (!indexSchema->addField(fieldHash[fieldName])) {
+                    delete indexSchema;
+                    qDeleteAll(indexList);
+                    return QList<KDbIndexSchema*>();
+                }
             }
         }
 

commit 3fb16fd4306dd05a8dd6128b4e56621f6fcaa395

Fix regression (creating userdata system table)
introduced in 4a1f5a16532e45 "Update for the recent addField/insertField API changes in KDb"

src/core/kexiproject.cpp
@@ -607,9 +607,9 @@ bool KexiProject::createInternalStructures(bool insideTransaction)
     // User data storage
     KDbInternalTableSchema *t_userdata = new KDbInternalTableSchema("kexi__userdata");
     t_userdata->addField(new KDbField("d_user", KDbField::Text, KDbField::NotNull));
-    t_parts->addField(new KDbField("o_id", KDbField::Integer, KDbField::NotNull, KDbField::Unsigned));
-    t_parts->addField(new KDbField("d_sub_id", KDbField::Text, KDbField::NotNull | KDbField::NotEmpty));
-    t_parts->addField(new KDbField("d_data", KDbField::LongText));
+    t_userdata->addField(new KDbField("o_id", KDbField::Integer, KDbField::NotNull, KDbField::Unsigned));
+    t_userdata->addField(new KDbField("d_sub_id", KDbField::Text, KDbField::NotNull | KDbField::NotEmpty));
+    t_userdata->addField(new KDbField("d_data", KDbField::LongText));
 
     const tristate containsKexi__userdataTable = d->connection->drv_containsTable("kexi__userdata");
     if (~containsKexi__userdataTable) {