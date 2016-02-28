0e2f182598fc945083b6d4bf4dd506d786394363
Jaroslaw Staniek <staniek@kde.org>
2016-02-28 16:57:41 +0100

    Data source combo box: clear button handling not needed since 'clear' action is supported inside of our combo box already

--- src/widget/KexiDataSourceComboBox.cpp
+++ src/widget/KexiDataSourceComboBox.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2005-2010 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2005-2016 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -274,15 +274,8 @@ void KexiDataSourceComboBox::slotActivated(int index)
 
 void KexiDataSourceComboBox::slotTextChanged(const QString &text)
 {
-    if (text.isEmpty()) {
-        slotClearButtonClicked();
-    }
-}
-
-void KexiDataSourceComboBox::slotClearButtonClicked()
-{
-    setCurrentIndex(0);
-    slotActivated(0);
+    Q_UNUSED(text)
+    //! @todo This place may be useful when we alow to enter values not being on the list
 }
 
 QString KexiDataSourceComboBox::selectedPluginId() const
--- src/widget/KexiDataSourceComboBox.h
+++ src/widget/KexiDataSourceComboBox.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2005-2010 Jarosław Staniek <staniek@kde.org>
+   Copyright (C) 2005-2016 Jarosław Staniek <staniek@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -80,7 +80,6 @@ protected Q_SLOTS:
     void slotActivated(int index);
     void slotReturnPressed(const QString & text);
     void slotTextChanged(const QString &text);
-    void slotClearButtonClicked();
 
 protected:
     virtual void focusOutEvent(QFocusEvent *e);
