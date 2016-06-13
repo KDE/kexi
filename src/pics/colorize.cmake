ea330bbe85c66c7a1f2a7240e674a11a3cf7f4ad
Jaroslaw Staniek <staniek@kde.org>
2016-06-13 13:49:53 +0200

    Colorize all kexi icons, will be useful in dark panes
    + group icons names a bit more in cmake

--- src/pics/colorize.cmake
+++ src/pics/colorize.cmake
@@ -1,18 +1,39 @@
 #
-# Colorizes Kexi object icons for dark backgrounds
+# Colorizes Kexi object icons for alternative backgrounds
 #
 
-macro(_colorize _name _size _oldcolor _color)
-    file(READ icons/breeze/actions/${_size}/${_name}.svg _SVG)
+include(kexi_breeze_files.cmake)
+
+macro(_colorize _filepath _oldcolor _color _colorized_filepath)
+    file(READ ${_filepath} _SVG)
     string(REPLACE "fill:#${_oldcolor}" "fill:#${_color}" _NEW_SVG "${_SVG}")
-    file(WRITE icons/breeze/actions/${_ICON_SIZE}/${_name}@dark.svg "${_NEW_SVG}")
+    string(REPLACE ".svg" "@dark.svg" "${_colorized_filepath}" "${_filepath}")
+    file(WRITE "${${_colorized_filepath}}" "${_NEW_SVG}")
+    MESSAGE(STATUS "_filepath=${_filepath}")
+    MESSAGE(STATUS "_colorized_filepath=${${_colorized_filepath}}")
 endmacro()
 
+macro(_colorize_list _filepath _oldcolor _color _colorized_files)
+    _colorize(${_filepath} ${_oldcolor} ${_color} _colorized_filepath)
+    list(APPEND "${_colorized_files}" "${_colorized_filepath}")
+endmacro()
+
+# Special cases
 foreach(_ICON_SIZE 16 22 32)
-    _colorize("table" ${_ICON_SIZE} "2c3e50" "a0b5cb")
-    _colorize("query" ${_ICON_SIZE} "2c3e50" "a0b5cb")
-    _colorize("form" ${_ICON_SIZE} "2980b9" "39b0ff")
-    _colorize("report" ${_ICON_SIZE} "27ae60" "26d26f")
-    _colorize("macro" ${_ICON_SIZE} "8e44ad" "d164ff")
-    _colorize("script" ${_ICON_SIZE} "d35400" "ff771c")
+    _colorize_list("icons/breeze/actions/${_ICON_SIZE}/table.svg"  "2c3e50" "a0b5cb" _SVG_COLORIZED_OBJECT_FILES)
+    _colorize_list("icons/breeze/actions/${_ICON_SIZE}/query.svg"  "2c3e50" "a0b5cb" _SVG_COLORIZED_OBJECT_FILES)
+    _colorize_list("icons/breeze/actions/${_ICON_SIZE}/form.svg"   "2980b9" "39b0ff" _SVG_COLORIZED_OBJECT_FILES)
+    _colorize_list("icons/breeze/actions/${_ICON_SIZE}/report.svg" "27ae60" "26d26f" _SVG_COLORIZED_OBJECT_FILES)
+    _colorize_list("icons/breeze/actions/${_ICON_SIZE}/macro.svg"  "8e44ad" "d164ff" _SVG_COLORIZED_OBJECT_FILES)
+    _colorize_list("icons/breeze/actions/${_ICON_SIZE}/script.svg" "d35400" "ff771c" _SVG_COLORIZED_OBJECT_FILES)
 endforeach()
+
+set(ICONGREY_COLOR "4d4d4d")
+
+foreach(_SVG_FILE ${_SVG_FILES})
+    _colorize_list("${_SVG_FILE}" ${ICONGREY_COLOR} "7f8c8d" _SVG_COLORIZED_FILES)
+endforeach()
+
+#message(STATUS "_SVG_FILES=${_SVG_FILES}")
+#message(STATUS "_SVG_COLORIZED_OBJECT_FILES=${_SVG_COLORIZED_OBJECT_FILES}")
+#message(STATUS "_SVG_COLORIZED_FILES=${_SVG_COLORIZED_FILES}")
--- src/pics/update_kexi_breeze_list.sh
+++ src/pics/update_kexi_breeze_list.sh
@@ -3,5 +3,33 @@
 # Updates list of files for kexi_breeze.rcc.
 #
 
-(echo "set(_FILES"; find . -name \*svg -o -name \*png | sed "s/\.\///g" | sort; echo ")") \
-    > kexi_breeze_files.cmake
+function content()
+{
+#    echo "include(colorize.cmake)"
+#    echo
+    echo "set(_PNG_FILES"
+    find . -name \*png | sed "s/\.\///g" | sort
+    echo ")"
+    echo
+
+    echo "set(_SVG_OBJECT_FILES"
+    find . -regex ".*/\(table\|query\|form\|report\|macro\|script\)\.svg$" \
+        | sed "s/\.\///g" | sort
+    echo ")"
+    echo
+
+    echo "set(_SVG_FILES"
+    find . -name \*svg | grep -v -F "table.svg
+query.svg
+form.svg
+report.svg
+macro.svg
+script.svg" \
+        | sed "s/\.\///g" | sort
+    echo ")"
+    echo
+
+    echo "set(_FILES \${_PNG_FILES} \${_SVG_OBJECT_FILES} \${_SVG_FILES})"
+}
+
+content > kexi_breeze_files.cmake
