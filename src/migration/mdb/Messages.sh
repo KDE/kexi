478be48527b8b2e2dadfcc09c1ebb2b6bdc7caa0
Jaroslaw Staniek <staniek@kde.org>
2016-07-22 23:12:14 +0200

    migration: add missing Messages.sh, update names, add Messages.sh to sources

--- src/migration/mdb/Messages.sh
+++ src/migration/mdb/Messages.sh
@@ -5,9 +5,7 @@ potfile=keximigrate_mdb
 find_exclude $potfile
 
 # Exclude files containing "#warning noi18n"
-LIST=`find . \( $EXCLUDE \) -prune -o \( -name \*.h -o -name \*.cpp -o -name \*.cc -o -name \*.hxx -o -name \*.cxx \) -type f -print \
-| egrep "kexidb/drivers/mysql/|migration/mysql/" \
-| sort | while read f ; do \
+LIST=`find . \( $EXCLUDE \) -prune -o \( -name \*.h -o -name \*.cpp -o -name \*.cc -o -name \*.hxx -o -name \*.cxx \) -type f -print | sort | while read f ; do \
     if ! grep -q '^#warning noi18n ' $f ; then echo $f; fi \
 done \
 `
