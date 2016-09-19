6887691714c501a8e5cc7c56c6abd6b19a14ef7e
Jaroslaw Staniek <staniek@kde.org>
2016-09-19 14:25:02 +0200

    Set Kexi version to Version 2.99.91, use Kexi frameworks 2.99.3

--- cmake/modules/SetKexiVersionInfo.cmake
+++ cmake/modules/SetKexiVersionInfo.cmake
@@ -8,7 +8,7 @@
 set(KEXI_VERSION_STRING "3.0 Alpha")
 set(KEXI_STABLE_VERSION_MAJOR 3) # 3 for 3.x, 4 for 4.x, etc.
 set(KEXI_STABLE_VERSION_MINOR 0) # 0 for 3.0, 1 for 3.1, etc.
-set(KEXI_VERSION_RELEASE 90)     # 89 for Alpha, increase for next test releases, set 0 for first Stable, etc.
+set(KEXI_VERSION_RELEASE 91)     # 89 for Alpha, increase for next test releases, set 0 for first Stable, etc.
 set(KEXI_ALPHA 1) # uncomment only for Alpha
 #set(KEXI_BETA 1) # uncomment only for Beta
 #set(KEXI_RC 1) # uncomment only for RC
