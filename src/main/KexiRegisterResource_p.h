d8b6bfd4884757a64324a246a556e367a1f6af1c
Jaroslaw Staniek <staniek@kde.org>
2016-11-14 02:50:57 +0100

    TRIVIAL: Add routines to KexiRegisterResource_p.h that can be used to load in plugins
    (like in KReport)

--- src/main/KexiRegisterResource_p.h
+++ src/main/KexiRegisterResource_p.h
@@ -27,7 +27,14 @@
 
 #ifdef QT_ONLY
 #define KLocalizedString QString
+#else
+#include <KConfigGroup>
+#include <KSharedConfig>
+#include <KMessageBox>
 #endif
+
+//! @todo Support other themes
+const QString supportedIconTheme = QLatin1String("breeze");
 
 //! @return true if @a path is readable
 static bool fileReadable(const QString &path)
@@ -41,7 +48,33 @@ static bool fileReadable(const QString &path)
 #define KPATH_SEPARATOR ':'
 #endif
 
-static QString locateFile(const QString& path, QStandardPaths::StandardLocation location,
+//! @brief Used for a workaround: locations for QStandardPaths::AppDataLocation end with app name.
+//! If this is not an expected app but for example a test app, replace
+//! the subdir name with app name so we can find resource file(s).
+static QStringList correctStandardLocations(const QString &privateName,
+                                     QStandardPaths::StandardLocation location,
+                                     const QString &extraLocation)
+{
+    QStringList result;
+    if (!privateName.isEmpty()) {
+        QRegularExpression re(QLatin1Char('/') + QCoreApplication::applicationName() + QLatin1Char('$'));
+        QStringList standardLocations(QStandardPaths::standardLocations(location));
+        if (!extraLocation.isEmpty()) {
+            standardLocations.append(extraLocation);
+        }
+        for(const QString &dir : standardLocations) {
+            if (dir.indexOf(re) != -1) {
+                QString realDir(dir);
+                realDir.replace(re, QLatin1Char('/') + privateName);
+                result.append(realDir);
+            }
+        }
+    }
+    return result;
+}
+
+static QString locateFile(const QString &privateName,
+                          const QString& path, QStandardPaths::StandardLocation location,
                           const QString &extraLocation)
 {
     // let QStandardPaths handle this, it will look for app local stuff
@@ -74,33 +107,29 @@ static QString locateFile(const QString& path, QStandardPaths::StandardLocation 
         }
     }
 
-    // A workaround: locations for QStandardPaths::AppDataLocation end with app name.
-    // If this is not a kexi app but a test app such as GlobalSearchTest, replace
-    // the subdir name with "kexi" so we can find Kexi file(s).
-    QRegularExpression re(QLatin1Char('/') + QCoreApplication::applicationName() + '$');
-    QStringList standardLocations(QStandardPaths::standardLocations(location));
-    if (!extraLocation.isEmpty()) {
-        standardLocations.append(extraLocation);
-    }
-    for(const QString &dir : standardLocations) {
-        if (dir.indexOf(re) != -1) {
-            QString realDir(dir);
-            realDir.replace(re, QLatin1String("/kexi"));
-            fullPath = realDir + '/' + path;
-            if (fileReadable(fullPath)) {
-                break;
-            }
+    const QStringList correctedStandardLocations(correctStandardLocations(privateName, location, extraLocation));
+    for(const QString &dir : correctedStandardLocations) {
+        fullPath = QFileInfo(dir + QLatin1Char('/') + path).canonicalFilePath();
+        if (fileReadable(fullPath)) {
+            return fullPath;
         }
     }
     return fullPath;
 }
 
+#ifdef KEXI_BASE_NAME_LOWER
+#define BASE_NAME KEXI_BASE_NAME_LOWER
+#else
+#define BASE_NAME QCoreApplication::applicationName()
+#endif
+
 static bool registerResource(const QString& path, QStandardPaths::StandardLocation location,
                              const QString &resourceRoot, const QString &extraLocation,
                              KLocalizedString *errorMessage, KLocalizedString *detailsErrorMessage)
 {
-    const QString fullPath = locateFile(path, location, extraLocation);
-    if (fullPath.isEmpty() || !QFileInfo(fullPath).isReadable()
+    const QString fullPath = locateFile(BASE_NAME,
+                                        path, location, extraLocation);
+    if (fullPath.isEmpty()
         || !QResource::registerResource(fullPath, resourceRoot))
     {
         QStringList triedLocations(QStandardPaths::standardLocations(location));
@@ -150,3 +179,163 @@ static void setupBreezeIconTheme()
     QIcon::setThemeName(QStringLiteral("breeze"));
 #endif
 }
+
+/*! @brief Registers icons resource file
+ * @param privateName Name to be used instead of application name for resource lookup
+ * @param path Relative path to the resource file
+ * @param location Standard file location to use for file lookup
+ * @param resourceRoot A resource root for QResource::registerResource()
+ * @param errorMessage On failure it is set to a brief error message.
+ * @param errorDescription On failure it is set to a detailed error message.
+ * other for warning
+ */
+static bool registerIconsResource(const QString &privateName, const QString& path,
+                             QStandardPaths::StandardLocation location,
+                             const QString &resourceRoot, const QString &extraLocation,
+                             QString *errorMessage, QString *detailedErrorMessage)
+{
+    const QString fullPath = locateFile(privateName, path, location, extraLocation);
+    if (fullPath.isEmpty() || !QFileInfo(fullPath).isReadable()
+        || !QResource::registerResource(fullPath, resourceRoot))
+    {
+        QStringList triedLocations(QStandardPaths::standardLocations(location));
+        if (!extraLocation.isEmpty()) {
+            triedLocations.append(extraLocation);
+        }
+        triedLocations.append(correctStandardLocations(privateName, location, extraLocation));
+        const QString triedLocationsString = QLocale().createSeparatedList(triedLocations);
+#ifdef QT_ONLY
+        *errorMessage = QString("Could not open icon resource file %1.").arg(path);
+        *detailedErrorMessage = QString("Tried to find in %1.").arg(triedLocationsString);
+#else
+        //! @todo 3.1 Re-add translation
+        *errorMessage = /*QObject::tr*/ QString::fromLatin1(
+            "Could not open icon resource file \"%1\". "
+            "Application will not start. "
+            "Please check if it is properly installed.")
+            .arg(QFileInfo(path).fileName());
+        //! @todo 3.1 Re-add translation
+        *detailedErrorMessage = QString::fromLatin1("Tried to find in %1.").arg(triedLocationsString);
+#endif
+        return false;
+    }
+    *errorMessage = QString();
+    *detailedErrorMessage = QString();
+    return true;
+}
+
+#ifndef QT_ONLY
+/*! @brief Sets up a private icon resource file
+ * @return @c false on failure and sets error message. Does not warn or exit on failure.
+ * @param privateName Name to be used instead of application name for resource lookup
+ * @param path Relative path to the resource file
+ * @param themeName Icon theme to use. It affects filename.
+ * @param errorMessage On failure it is set to a brief error message
+ * @param errorDescription On failure it is set to a detailed error message
+ * other for warning
+ * @param prefix Resource path prefix. The default is useful for library-global resource,
+ * other values is useful for plugins.
+ */
+static bool setupPrivateIconsResource(const QString &privateName, const QString& path,
+                               const QString &themeName,
+                               QString *errorMessage, QString *detailedErrorMessage,
+                               const QString &prefix = QLatin1String(":/icons"))
+{
+    // Register application's resource first to have priority over the theme.
+    // Some icons may exists in both resources.
+    if (!registerIconsResource(privateName, path,
+                          QStandardPaths::AppDataLocation,
+                          QString(), QString(), errorMessage, detailedErrorMessage))
+    {
+        return false;
+    }
+    bool changeTheme = false;
+#ifdef QT_GUI_LIB
+    QIcon::setThemeSearchPaths(QStringList() << prefix << QIcon::themeSearchPaths());
+    changeTheme = 0 != QIcon::themeName().compare(themeName, Qt::CaseInsensitive);
+    if (changeTheme) {
+        QIcon::setThemeName(themeName);
+    }
+#endif
+
+    KConfigGroup cg(KSharedConfig::openConfig(), "Icons");
+    changeTheme = changeTheme || 0 != cg.readEntry("Theme", QString()).compare(themeName, Qt::CaseInsensitive);
+    // tell KIconLoader an co. about the theme
+    if (changeTheme) {
+        cg.writeEntry("Theme", themeName);
+        cg.sync();
+    }
+    return true;
+}
+
+/*! @brief Sets up a private icon resource file
+ * @return @c false on failure and sets error message.
+ * @param privateName Name to be used instead of application name for resource lookup
+ * @param path Relative path to the resource file
+ * @param themeName Icon theme to use. It affects filename.
+ * @param errorMessage On failure it is set to a brief error message.
+ * @param errorDescription On failure it is set to a detailed error message.
+ * other for warning
+ * @param prefix Resource path prefix. The default is useful for library-global resource,
+ * other values is useful for plugins.
+ */
+static bool setupPrivateIconsResourceWithMessage(const QString &privateName, const QString& path,
+                                          const QString &themeName,
+                                          QString *errorMessage, QString *detailedErrorMessage,
+                                          const QString &prefix = QLatin1String(":/icons"))
+{
+    if (!setupPrivateIconsResource(privateName, path, themeName,
+                                   errorMessage, detailedErrorMessage, prefix))
+    {
+        if (detailedErrorMessage->isEmpty()) {
+            KMessageBox::error(nullptr, *errorMessage);
+        } else {
+            KMessageBox::detailedError(nullptr, *errorMessage, *detailedErrorMessage);
+        }
+        return false;
+    }
+    return true;
+}
+
+/*! @overload setupPrivateIconsResourceWithMessage(QString &privateName, const QString& path,
+                                          const QString &themeName,
+                                          QString *errorMessage, QString *detailedErrorMessage,
+                                          const QString &prefix = QLatin1String(":/icons"))
+    Uses default theme name.
+ */
+static bool setupPrivateIconsResourceWithMessage(const QString &privateName, const QString& path,
+                                          QString *errorMessage, QString *detailedErrorMessage,
+                                          const QString &prefix = QLatin1String(":/icons"))
+{
+    return setupPrivateIconsResourceWithMessage(privateName, path, supportedIconTheme,
+                                                errorMessage, detailedErrorMessage, prefix);
+}
+
+/*! @brief Sets up a private icon resource file
+ * Warns on failure and returns @c false.
+ * @param privateName Name to be used instead of application name for resource lookup
+ * @param path Relative path to the resource file
+ * @param messageType Type of message to use on error, QtFatalMsg for fatal exit and any
+ * other for warning
+ * @param prefix Resource path prefix. The default is useful for library-global resource,
+ * other values is useful for plugins.
+ */
+static bool setupPrivateIconsResourceWithMessage(const QString &privateName, const QString& path,
+                                          QtMsgType messageType,
+                                          const QString &prefix = QLatin1String(":/icons"))
+{
+    QString errorMessage;
+    QString detailedErrorMessage;
+    if (!setupPrivateIconsResourceWithMessage(privateName, path,
+                                              &errorMessage, &detailedErrorMessage, prefix)) {
+        if (messageType == QtFatalMsg) {
+            qFatal("%s %s", qPrintable(errorMessage), qPrintable(detailedErrorMessage));
+        } else {
+            qWarning() << qPrintable(errorMessage) << qPrintable(detailedErrorMessage);
+        }
+        return false;
+    }
+    return true;
+}
+
+#endif // !QT_ONLY
