464196eace53272587cef044281de715c4a54451
Jaroslaw Staniek <staniek@kde.org>
2017-04-04 01:26:00 +0200

    Fix building WHERE conditions in query designers
    - fixed typo while porting to Kexi 3
    - check for null WHERE expression instead of valid expression because validity can't be computed so early
    
    FIXED-IN:3.0.2
    BUG:377579

--- src/plugins/queries/kexiquerydesignerguieditor.cpp
+++ src/plugins/queries/kexiquerydesignerguieditor.cpp
@@ -442,7 +442,7 @@ KexiQueryDesignerGuiEditor::buildSchema(QString *errMsg)
                 KDbToken token;
                 KDbExpression criteriaExpr = parseExpressionString(criteriaStr, &token,
                                                  true/*allowRelationalOperator*/);
-                if (criteriaExpr.isValid()) {//for sanity
+                if (!criteriaExpr.isValid()) {//for sanity
                     if (errMsg)
                         *errMsg = xi18nc("@info",
                                          "Invalid criteria <icode>%1</icode>", criteriaStr);
@@ -452,10 +452,11 @@ KexiQueryDesignerGuiEditor::buildSchema(QString *errMsg)
                 KDbVariableExpression varExpr(fieldAndTableName);
                 criteriaExpr = KDbBinaryExpression(varExpr, token, criteriaExpr);
                 //critera ok: add it to WHERE section
-                if (whereExpr.isValid())
-                    whereExpr = KDbBinaryExpression(whereExpr, KDbToken::AND, criteriaExpr);
-                else //first expr.
+                if (whereExpr.isNull()) {
                     whereExpr = criteriaExpr;
+                } else { //first expr.
+                    whereExpr = KDbBinaryExpression(whereExpr, KDbToken::AND, criteriaExpr);
+                }
             }
             if (tableName.isEmpty()) {
                 if ((*set)["isExpression"].value().toBool() == true) {
