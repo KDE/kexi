233e1d7e4b0f7666eb88b431a6db2908aa65a49e
Jaroslaw Staniek <staniek@kde.org>
2017-05-22 18:56:03 +0200

    GIT_SILENT Add Scarlett Gately Clark contributor
    Thanks Scarlett!
    
    CCMAIL:scarlett.gately.clark@gmail.com

--- src/core/kexiaboutdata.cpp
+++ src/core/kexiaboutdata.cpp
@@ -92,6 +92,8 @@ KexiAboutData::KexiAboutData()
         xi18n("Till Busch"), xi18n("Bugfixes, original Table Widget"), "till@bux.at");
 
     addCredit(
+        xi18n("Scarlett Gately Clark"), xi18n("AppImage packages for Linux"), "scarlett.gately.clark@gmail.com");
+    addCredit(
         xi18n("Ian Balchin"), xi18n("Numerous bug reports and tests, handbook improvements"), "inksi@fables.co.za");
     addCredit(
         xi18n("Robert Leleu"), xi18n("Numerous bug reports and tests"), "robert.jean.leleu@wanadoo.fr");
